3345acd088663b8c3e97fa54c2dc5a8e8b3685b9
unknown <mostafaswifyy@gmail.com>
2021-11-28 12:23:43 +0800

    update style

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -8,10 +8,12 @@ module.exports.homePage = async(req,res)=>{
 
 
 module.exports.search = async(req, res)=>{
+    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=b425506227934b86818152110212711&q=Malaysia&aqi=no');
+    const weatherData = await response.json();
     let searchTerm = req.body.searchInput
     async function grabResult(){
         const result = await shoppe.scrapingScript(searchTerm)
-        res.render('result', {result})
+        res.render('result', {result, weatherData})
     }
     try{
        
